ca89d8b1c8103276191d95050bc7874d3cabbfcd
Calebe Felipe Alves Freitas <fcalebe7@gmail.com>
2022-09-14 11:34:41 -0300

    feat: adding data validation and showing error alert

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
 
 // variáveis - variables
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
+const alertError = document.querySelector('.alert-error')
 
+//Quando for usado essa sintaxe para evento é possível utilizar apenas uma vez para o elemento selecionado
 form.onsubmit = event => {
     event.preventDefault()
 
     const weight = inputWeight.value 
     const height = inputHeight.value 
+    const showAlertError = notANumber(weight) || notANumber(height)
+
+    if(showAlertError) {
+        AlertError.open()
+        return;
+    }
+
+    AlertError.close()
 
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
@@ -18,6 +29,9 @@ form.onsubmit = event => {
     Modal.open()
 }
 
+function notANumber(value) {
+    return isNaN(value) || value === ""
+}
 
 const IMC = (weight, height) => (weight / ((height / 100) ** 2)).toFixed(2)
 
