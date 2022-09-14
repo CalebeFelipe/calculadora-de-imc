84bce696944fa34fa9dc23e487880247ef287e8a
Calebe Felipe Alves Freitas <fcalebe7@gmail.com>
2022-09-14 11:49:54 -0300

    feat: adding refactoring

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { Modal } from './modal.js'
 import { AlertError } from './alert-error.js'
+import { notANumber, calculateIMC } from './utils.js'
 
 // variáveis - variables
 const form = document.querySelector('form')
@@ -13,25 +14,25 @@ form.onsubmit = event => {
 
     const weight = inputWeight.value 
     const height = inputHeight.value 
-    const showAlertError = notANumber(weight) || notANumber(height)
+    const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
 
-    if(showAlertError) {
+    if(weightOrHeightIsNotANumber) {
         AlertError.open()
         return;
     }
 
     AlertError.close()
 
-    const result = IMC(weight, height)
-    const message = `Seu IMC é de ${result}`
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+}
 
+function displayResultMessage(result) {
+    const message = `Seu IMC é de ${result}`
+    
     Modal.message.innerText = message
     Modal.open()
 }
 
-function notANumber(value) {
-    return isNaN(value) || value === ""
-}
 
-const IMC = (weight, height) => (weight / ((height / 100) ** 2)).toFixed(2)
 
