690a5ef6d345d186414c4cff937684a756fc5e55
Calebe Felipe Alves Freitas <fcalebe7@gmail.com>
2022-09-14 10:38:37 -0300

    feat: creating a data structure for the modal

--- main.js
+++ main.js
@@ -2,9 +2,20 @@
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal .title button')
+
+// estrutura de dados que facilita a manutenção e a leitura -> Um objeto literal
+const Modal = {
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal .title button'),
+
+    open() {
+        Modal.wrapper.classList.add('open')
+    },
+    close() {
+        Modal.wrapper.classList.remove('open')
+    }
+}
 
 form.onsubmit = event => {
     event.preventDefault()
@@ -15,11 +26,11 @@ form.onsubmit = event => {
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
 
-    modalMessage.innerText = message
-    modalWrapper.classList.add('open')
+    Modal.message.innerText = message
+    Modal.open()
 }
 
-modalBtnClose.onclick = () => modalWrapper.classList.remove('open')
+Modal.buttonClose.onclick = () => Modal.close()
 
 
 const IMC = (weight, height) => (weight / ((height / 100) ** 2)).toFixed(2)
